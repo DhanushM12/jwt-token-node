14ed9f6b6d26acbb609b86eb5bab51669203470f
DhanushM12 <dhanushmukhi@gmail.com>
2022-06-05 13:13:46 +0530

    added middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 8000;
 
@@ -7,6 +8,36 @@ app.get('/', (req, res) => {
         message: "Hello everyone"
     })
 })
+
+app.post('/tokenGeneration', (req, res) => {
+    const user = {
+        id: 1,
+        username: 'octbatch',
+        email: 'oct@gmail.com'
+    }
+    jwt.sign(user, 'secretkey', function(err, token) {
+        res.json({
+            token
+        })
+      });
+})
+
+app.post('/tokenVerification', takeToken,  (req, res) => {
+
+})
+// middleware
+function takeToken(req, res, next){
+    const bearerHeader = req.headers['authorization']; // Bearer token
+    if(bearerHeader !== undefined){
+        const bearer = bearerHeader.split(' '); // ['bearer', 'token']
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+    else{
+        res.sendStatus(403);
+    }
+}
 
 app.listen(port, function(err){
     if(err){
